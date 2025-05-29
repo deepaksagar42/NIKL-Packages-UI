7e6c82da2752394b7029e55679729b0680010042
Neko Nik <nikhil@nekonik.com>
2025-05-29 15:31:57 +0530

    feat: add tabs for package details in PublicPackageDetails component

--- src/pages/PublicPackageDetails.tsx
+++ src/pages/PublicPackageDetails.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@radix-ui/themes";
+import { Box, Tabs, Text } from "@radix-ui/themes";
 import React from "react";
 import { NavBar } from "../components/NavBar";
 import { useParams } from "react-router-dom";
@@ -15,6 +15,31 @@ export const PublicPackageDetails: React.FC = () => {
       <h1>PublicPackageDetails</h1>
       <p>Package ID: {packageId}</p>
       <p>Package Version: {packageVersion}</p>
+
+      {/* Need UI exactly lilke : https://crates.io/crates/syn/2.0.101 */}
+
+      <Tabs.Root defaultValue="readme">
+        <Tabs.List>
+          <Tabs.Trigger value="readme">Readme</Tabs.Trigger>
+          <Tabs.Trigger value="versions">Versions</Tabs.Trigger>
+          <Tabs.Trigger value="dependencies">Dependencies</Tabs.Trigger>
+        </Tabs.List>
+
+        <Box pt="3">
+          <Tabs.Content value="readme">
+            <Text size="2">Readme content goes here</Text>
+          </Tabs.Content>
+
+          <Tabs.Content value="versions">
+            <Text size="2">List of versions for this package</Text>
+          </Tabs.Content>
+
+          <Tabs.Content value="dependencies">
+            <Text size="2">Dependencies for this package will be listed here</Text>
+          </Tabs.Content>
+        </Box>
+      </Tabs.Root>
+
 
     </Box>
   );
