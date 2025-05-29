ccdeb989bfc3dc36e6a113eb6e47a2ec94688870
Neko Nik <nikhil@nekonik.com>
2025-05-29 15:25:47 +0530

    feat: add package ID and version display in PublicPackageDetails component

--- src/pages/PublicPackageDetails.tsx
+++ src/pages/PublicPackageDetails.tsx
@@ -1,14 +1,20 @@
 import { Box } from "@radix-ui/themes";
 import React from "react";
 import { NavBar } from "../components/NavBar";
+import { useParams } from "react-router-dom";
 
 
 export const PublicPackageDetails: React.FC = () => {
+  // Get the Package ID and Version from the URL path /package/:id/:version
+  const { id: packageId, version: packageVersion } = useParams<{ id: string; version: string }>();
+
   return (
     <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <NavBar />
 
       <h1>PublicPackageDetails</h1>
+      <p>Package ID: {packageId}</p>
+      <p>Package Version: {packageVersion}</p>
 
     </Box>
   );
