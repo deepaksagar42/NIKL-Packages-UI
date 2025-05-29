2629d2e628d7d3715ac1c22ac73ecaabc51d522d
Neko Nik <nikhil@nekonik.com>
2025-05-29 16:23:39 +0530

    feat: enhance Login component with structured layout and input fields

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
-import { Box, Button } from "@radix-ui/themes";
+import { Box, Button, Card, Checkbox, Flex, Heading, Text, TextField } from "@radix-ui/themes";
 import { csrfToken } from '../state/Auth';
 import { useSetAtom } from "jotai";
 
@@ -12,18 +12,35 @@ export const Login: React.FC = () => {
     <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <NavBar />
 
-      <h1>Login Page</h1>
+      <Heading size="5" style={{ marginTop: '20px' }}>
+        Login to Your Account
+      </Heading>
 
-      <Button
-        variant="solid"
-        color="mint"
-        onClick={() => {
-          setCsrfToken('mock-csrf-token'); // Set a mock CSRF token
-          window.location.href = '/manage/dashboard'; // Redirect to dashboard after login
-        }}
-      >
-        Login
-      </Button>
+      <Box style={{ marginTop: '20px' }}>
+        <Card style={{ padding: '20px', width: '400px', textAlign: 'center' }}>
+          <TextField.Root radius="full" placeholder="Username" style={{ width: '300px', marginBottom: '10px' }} />
+          <TextField.Root radius="full" placeholder="Password" type="password" style={{ width: '300px' }} />
+
+          <Text as="label" size="2">
+            <Flex gap="2">
+              <Checkbox defaultChecked />
+              Show Password
+            </Flex>
+          </Text>
+
+          <Button
+            variant="solid"
+            color="mint"
+            onClick={() => {
+              setCsrfToken('mock-csrf-token'); // Set a mock CSRF token
+              window.location.href = '/manage/dashboard'; // Redirect to dashboard after login
+            }}
+            >Login
+          </Button>
+
+        </Card>
+      </Box>
+
       <p>Click the button to simulate login and redirect to the dashboard.</p>
       <p>Note: This is a simulated login for demonstration purposes.</p>
 
